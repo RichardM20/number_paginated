0b00987e24e648a2b109122b00d8d8c8724dd954
RichardM20 <richardoficial2020@gmail.com>
2025-05-28 23:21:42 -0500

    feat: topics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 # v1.0.0 [28-05-2025]
 
 * TODO: The package is published with the initial stable release.
+
+## v1.0.1 [28-05-2025]
+
+* topics added
+* repository added
+* readme fixed
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
     title: 'Flutter Pagination Demo',
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-    ),
+    
     home: const PaginatedTabsPage(),
   );
 }
