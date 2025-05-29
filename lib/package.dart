1883edd9587f7b41078f90973c6dd1d05b519b7b
RichardM20 <64317751+RichardM20@users.noreply.github.com>
2025-05-29 04:24:02 +0000

    Deploying to gh-pages from @ RichardM20/number_paginated@94c22ff637edc39526da66eeb4b092fa0c28e168 🚀

--- lib/package.dart
+++
@@ -1,5 +0,0 @@
-library;
-
-export 'package:number_paginated_list/src/models/properties.dart';
-export 'package:number_paginated_list/src/ui/number_paginated.dart';
-export 'package:number_paginator/number_paginator.dart';
